9e8effe2f0c3445cdbdf4a53296ed0b5b99bba49
Bulygin Aleksey <alex987@bk.ru>
2022-12-26 17:11:48 +0300

    Github Pages routing in react SPA. React-toastify.

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,14 @@
+import React from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
   return (
     <>
       <div className="btn btn-primary m-3">
         <h1>App</h1>
       </div>
+      <ToastContainer />
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/App";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/gains-vs-spending">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
