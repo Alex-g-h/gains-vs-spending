f4b1c7e0a3204146f084bf9e7e5f213dd3136921
Bulygin Aleksey <alex987@bk.ru>
2023-02-17 20:57:50 +0300

    [feat]: refactoring service

--- src/app/services/account.service.js
+++ src/app/services/account.service.js
@@ -1,29 +1,5 @@
-import httpService from "./http.service";
+import commonService from "./common.service";
 
-const accountEndpoint = "account/";
+const accountService = commonService("account/");
 
-const accountService = {
-  get: async () => {
-    const { data } = await httpService.get(accountEndpoint);
-    return data;
-  },
-  create: async (payload) => {
-    const { data } = await httpService.put(
-      accountEndpoint + payload._id,
-      payload
-    );
-    return data;
-  },
-  delete: async (id) => {
-    const { data } = await httpService.delete(accountEndpoint + id);
-    return data;
-  },
-  update: async (payload) => {
-    const { data } = await httpService.patch(
-      accountEndpoint + payload._id,
-      payload
-    );
-    return data;
-  },
-};
 export default accountService;
