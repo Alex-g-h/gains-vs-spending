a80da869df99e2ce0f08853dd0a0d645b374ca50
Bulygin Aleksey <alex987@bk.ru>
2023-02-23 19:34:10 +0300

    [chore]: Small commit to run github action

--- src/app/utils/makeAccountNumberSecure.js
+++ src/app/utils/makeAccountNumberSecure.js
@@ -1,6 +1,7 @@
 const makeAccountNumberSecure = (number) => {
   const numberStr = String(number);
   const prefix = "***";
+
   const lastDigits = numberStr.substring(numberStr.length - 4);
   return { prefix, lastDigits };
 };
