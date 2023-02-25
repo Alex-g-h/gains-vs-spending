4e806f6c1bb9d41c42b2002e928830ffdf9ccba6
Bulygin Aleksey <alex987@bk.ru>
2023-02-25 18:40:51 +0300

    [chore]: Add user route

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,11 +1,67 @@
 const express = require("express");
 const User = require("../models/User");
 const router = express.Router({ mergeParams: true });
+const auth = require("../middleware/auth.middleware");
 
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Server side error occurred. Try again later" });
+  }
+});
 
-router.put("/:id", async (req, res) => {});
+router.get("/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const user = await User.findById(userId);
 
-router.patch("/:id", async (req, res) => {});
+    if (!user) res.status(404).json({ message: "User not found" });
+
+    res.send(user);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Server side error occurred. Try again later" });
+  }
+});
+
+router.put("/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const newUser = await User.create({
+      ...req.body,
+      userId: userId,
+    });
+
+    res.status(201).send(newUser);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Server side error occurred. Try again later" });
+  }
+});
+
+router.patch("/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    if (userId === req.user._id) {
+      const updatedUser = await User.findByIdAndUpdate(userId, req.body, {
+        new: true,
+      });
+      res.send(updatedUser);
+    } else {
+      res.status(401).json({ message: "Unauthorized" });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Server side error occurred. Try again later" });
+  }
+});
 
 module.exports = router;
